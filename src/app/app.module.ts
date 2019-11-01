67a6a0f1eda99f59a4929982618c2cdbdd9eea15
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-11-01 15:35:42 -0600

    bug translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,10 @@ import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -63,7 +67,3 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-// required for AOT compilation
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
