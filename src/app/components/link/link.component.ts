220616a3922f9d9f19b575096636ad9059560452
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-11-11 14:28:53 -0600

    Funcionalidad del web service

--- src/app/components/link/link.component.ts
+++ src/app/components/link/link.component.ts
@@ -7,19 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./link.component.css']
 })
 export class LinkComponent implements OnInit {
+
   constructor( public router: Router) { }
 
   ngOnInit() {
   }
   close(numberLink) {
     console.log(numberLink);
-    window.location.href ='https://maat-ia-development.appspot.com/ws/hp/sendDownloadlink?cel=${numberLink}';
-    //this.router.navigate(['/']);
-    /*if (numberLink = '') {
-      console.log("error");
-    } else {
-
-    }*/
+    window.location.href =`https://maat-ia-development.appspot.com/ws/hp/sendDownloadlink?cel=${numberLink}`;
+    this.router.navigate(['/']);
     //https://maat-ia-development.appspot.com/ws/hp/sendDownloadlink?cel={{number}}
 
   }
