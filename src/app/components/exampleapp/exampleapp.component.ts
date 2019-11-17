6207d28fac1d0723071a532d3c01424b9b20d7b1
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-11-16 22:28:53 -0600

    err

--- src/app/components/exampleapp/exampleapp.component.ts
+++ src/app/components/exampleapp/exampleapp.component.ts
@@ -19,15 +19,16 @@ export class ExampleappComponent implements OnInit {
   changeImages() {
     this.detectLanguage = navigator.language;
     console.log(this.detectLanguage);
-    if(this.detectLanguage == 'es-419' || this.detectLanguage == 'es' || this.detectLanguage == 'es-US') {
+    if(this.detectLanguage == 'es-419' || this.detectLanguage == 'es' || this.detectLanguage == 'es-US' || this.detectLanguage == 'es-MX') {
       //document.getElementById('imgEn').style.display='none';
       this.visibleDivEs = true;
       //document.getElementById('imgEs').style.display='block';
-    }else if(this.detectLanguage == 'en-US' || this.detectLanguage == 'en'){
+    }else if(this.detectLanguage == 'en-US' || this.detectLanguage == 'en' || this.detectLanguage == 'en-CA'){
       this.visibleDivEn = true;
       //document.getElementById('imgEs').style.display='none';
       //document.getElementById('imgEn').style.display='block';
     }else {
+      this.visibleDivEs = true;
       console.log("error en carga de imagenes");
     }
   }
