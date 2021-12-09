4c908d1a7c0797885bae6c5cf89fe0cb3d5ab785
Ana Vázquez <ana.maria.vl@hotmail.com>
2021-12-09 14:56:42 -0600

    Comment tagmanager

--- src/app/components/download/download.component.ts
+++ src/app/components/download/download.component.ts
@@ -15,15 +15,14 @@ export class DownloadComponent implements OnInit {
   }
 
   detect(){
-    let osMobile = navigator.userAgent;
+    let osMobile = window.navigator.userAgent || navigator.vendor || navigator.userAgent;
     console.log(osMobile);
-    if(osMobile.includes('Android')){
+    if(osMobile.includes('Android') || /Android/i.test(osMobile) || navigator.userAgent.match(/Android/i) || /android/i.test(osMobile)){
       console.log("Si funciona");
       window.location.href = "https://play.google.com/store/apps/details?id=com.maatai.maatapp&hl=es-419&ah=pny3Z6zoy162CNptM1OYajk5NF0";
-    } else if(osMobile.includes('iPhone')) {
-      window.location.href = "https://apps.apple.com/us/app/maat-ai/id1482680056";
-    }
-    else{
+    } else if(osMobile.includes('iPhone') || /iPhone/i.test(osMobile) || navigator.userAgent.match(/iPhone/i)) {
+      window.location.href = "https://testflight.apple.com/join/0Zrc0HME";
+    } else{
       window.location.href = "https://www.maatai.com/link";
     }
   }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -44,9 +44,10 @@ export class HomeComponent implements OnInit {
     if(/Android/i.test(osMobile)){
       console.log("Si funciona");
       window.location.href = "https://play.google.com/store/apps/details?id=com.maatai.maatapp&hl=es-419&ah=pny3Z6zoy162CNptM1OYajk5NF0";
-    } else {
-      console.log("no funciono");
+    }  else if(osMobile.includes('iPhone') || /iPhone/i.test(osMobile)) {
       window.location.href = "https://testflight.apple.com/join/0Zrc0HME";
+    } else{
+      window.location.href = "https://www.maatai.com/link";
     }
   }
 
